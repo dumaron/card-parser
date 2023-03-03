a5fb8ca9b481ecb4a68a3ccf15e60a9cd40beb35
duma <dumaron@gmail.com>
2023-03-03 19:56:35 +0100

    added single test

--- tests/command_parsing/tags.test.ts
+++ tests/command_parsing/tags.test.ts
@@ -12,4 +12,14 @@ describe('Tags parsing', () => {
       expect(commands[0].tags).toEqual(['tag1', 'tag2', 'tag3'])
       expect(commands[0].cleanLine).not.toContain('tag')
    })
+
+   it('Should recognize the tags when mixed with other words', () => {
+      const command = '+TODO #tag1 "whatever" #tag2 project:test #tag3'
+      const commands = _parseCommandFile(command)
+
+      expect(commands.length).toEqual(1)
+      expect(commands[0] instanceof TodoCommand).toBeTruthy()
+      expect(commands[0].tags).toEqual(['tag1', 'tag2', 'tag3'])
+      expect(commands[0].cleanLine).not.toContain('tag')
+   })
 })
