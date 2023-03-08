e0fda742b73c5f769c19c95ce62d0718120fb734
duma <dumaron@gmail.com>
2023-03-08 21:43:18 +0100

    added missing file

--- utils/fs.ts
+++ utils/fs.ts
@@ -3,4 +3,3 @@ import { readFileSync, writeFileSync } from 'fs'
 export const readFile = (path: string) => readFileSync(path, {encoding: 'utf-8'})
 
 export const prependFileSync = (path: string, newContent: string) => writeFileSync(path, newContent + readFile(path))
-
